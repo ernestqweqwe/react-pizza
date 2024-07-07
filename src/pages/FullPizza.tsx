bfe9aec69a55695ac0a7818efb277fee784131e8
Ernestas Yushkiavichus <kenyrofa@gmail.com>
2024-07-07 19:15:59 +0300

    21 начало использования TS

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-const FullPizza = () => {
-  const [pizza, setPizza] = React.useState();
-  const { id } = useParams();// вытаскиваем динамические параметры из url, для не динамических используем хук useSearchParams, так же можно через хук Location(там объект и внутри свойство search)
+const FullPizza: React.FC = () => {
+  const [pizza, setPizza] = React.useState<{
+    imageUrl: string;
+    title: string;
+    price: number;
+  }>();
+  const { id } = useParams(); // вытаскиваем динамические параметры из url, для не динамических используем хук useSearchParams, так же можно через хук Location(там объект и внутри свойство search)
 
   React.useEffect(() => {
     async function fetchPizza() {
@@ -12,19 +16,19 @@ const FullPizza = () => {
         const { data } = await axios.get('https://661fa52416358961cd94ff2b.mockapi.io/items/' + id);
         setPizza(data);
       } catch (error) {
-        alert('ошибка при получении пиццы')
+        alert('ошибка при получении пиццы');
       }
     }
-    fetchPizza()
-  }, []);
+    fetchPizza();
+  });
 
-  if(!pizza) {
-    return 'Загрузка'
+  if (!pizza) {
+    return 'Загрузка';
   }
 
   return (
     <div>
-      <img src={pizza.imageUrl} />
+      <img src={pizza.imageUrl} alt={`pizzsImg${id}`} />
       <h2>{pizza.title}</h2>
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt ut tempore corrupti error
